587b80ca1903faf5cd2826af62035607c26f8c0b
Chen-Pang He <chen.pang.he@jdh8.org>
2017-08-08 15:49:05 +0800

    Improve code with widespread ES6 features

--- loader.js
+++ loader.js
@@ -2,27 +2,25 @@
 
 document.querySelector("main").appendChild(document.getElementById("loader").content);
 
-!function(){
-
-var button = document.getElementById("load");
-var client = new XMLHttpRequest();
-var count = 2;
-
-button.addEventListener("click", function()
 {
-	client.open("GET", "page/" + count + "/");
-	client.responseType = "document";
-	client.send();
-});
+	const button = document.getElementById("load");
+	const client = new XMLHttpRequest();
+	let count = 2;
 
-client.addEventListener("load", function()
-{
-	if ((this.status / 100)|0 == 2)
+	button.addEventListener("click", function()
 	{
-		button.hidden = ++count > paginator.pages;
-		button.before(...this.response.querySelectorAll("article.post"));
-		history.pushState(null, this.response.title, this.responseURL);
-	}
-});
+		client.open("GET", "page/" + count + "/");
+		client.responseType = "document";
+		client.send();
+	});
 
-}();
+	client.addEventListener("load", function()
+	{
+		if (((this.status|0) / 100)|0 == 2)
+		{
+			button.hidden = ++count > pages;
+			button.before(...this.response.querySelectorAll("article.post"));
+			history.pushState(null, this.response.title, this.responseURL);
+		}
+	});
+}
