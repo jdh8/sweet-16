432dd52c53bc9c8d79986d617fa5a0851f47bbfb
Chen-Pang He <chen.pang.he@jdh8.org>
2021-12-01 23:36:29 +0800

    ES6 module page loader

---
+++ load.js
@@ -0,0 +1,22 @@
+document.querySelector("main").appendChild(document.getElementById("loader").content);
+
+const button = document.getElementById("load");
+const client = new XMLHttpRequest();
+let count = 2;
+
+button.addEventListener("click", () =>
+{
+	client.open("GET", "page/" + count + "/");
+	client.responseType = "document";
+	client.send();
+});
+
+export default pages => client.addEventListener("load", function()
+{
+	if (((this.status|0) / 100)|0 == 2)
+	{
+		button.hidden = ++count > pages;
+		button.before(...this.response.querySelectorAll("article.post"));
+		history.pushState(null, this.response.title, this.responseURL);
+	}
+});
